6e8d9942c1425066cc9705eda9bf79b7b124d0ca
Mr <p176053@nu.edu.pk>
2018-02-27 11:47:11 +0500

    Added Comments & More...

--- rot13.cpp
+++ rot13.cpp
@@ -7,11 +7,11 @@ void encode();
 void decode();
 
 
-void choices()
+void choices() //Prompts Options to the user
   {
     int select;
     string option[] = {"1. Encode","2. Decode","3. Exit" };
-    cout<<option[0]<<endl<<option[1]<<endl<<option[2]<<endl<<"Please Select an option"<<endl;
+    cout<<option[0]<<endl<<option[1]<<endl<<option[2]<<endl<<"Please Select an option: ";
     cin>>select;
     cin.ignore();
     if(select == 1) encode();
@@ -23,33 +23,33 @@ int main()
     choices();
   }
 
-void encode()
+void encode()  //Starts Encoding
   {
     string msg;
     int key = 13;
-    cout<<"Please Enter the text to encode: ";
+    cout<<"Please Enter the plain text to Encrypt: ";
     getline(cin,msg);
 
     for(int i = 0; i < msg.size(); i++)
       {
-        if(msg[i] >= char(65) && msg[i] <= char(90) || msg[i] >= char(97) && msg[i] <= char(122))
-        cout << char(msg[i] + key);
-        else
+        if(msg[i] >= char(65) && msg[i] <= char(90) || msg[i] >= char(97) && msg[i] <= char(122))   //A-Z or a-z
+          cout << char(msg[i] + key);
+        else                                                //for special characters
           cout <<msg[i];
       }
   return;
   }
 
-void decode()
+void decode()  //Starts Decoding
   {
     string msg;
     int key = 13;
-    cout<<"Please Enter your Encrypted Message: ";
+      cout<<"Please Enter your Encrypted Message: ";
     getline(cin, msg);
     for(int i = 0; i < msg.size(); i++)
       {
         if(msg[i] > char(64) && msg[i] < char(93) || msg[i] > char(96) && msg[i] < char(123))
-        cout << char(msg[i] - key);
+          cout << char(msg[i] - key);
         else
           cout<<msg[i];
       }
