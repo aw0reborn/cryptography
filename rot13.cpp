01479db1c849513b0cd3427ee4c381404ef249c8
Mr <p176053@nu.edu.pk>
2018-02-27 10:42:28 +0500

    Rot13 Algorithm

--- rot13.cpp
+++ rot13.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 using namespace std;
 
 /*~~~~~ Declaring functions ~~~~~~ */
 void encode();
-//void decode();
+void decode();
 
 
 void choices()
@@ -13,8 +13,9 @@ void choices()
     string option[] = {"1. Encode","2. Decode","3. Exit" };
     cout<<option[0]<<endl<<option[1]<<endl<<option[2]<<endl<<"Please Select an option"<<endl;
     cin>>select;
+    cin.ignore();
     if(select == 1) encode();
-  //  if(select == 2) decode();
+    if(select == 2) decode();
   }
 
 int main()
@@ -25,10 +26,31 @@ int main()
 void encode()
   {
     string msg;
+    int key = 13;
     cout<<"Please Enter the text to encode: ";
-    cin>>msg;
-    for(int i = 0; i < sizeof(msg); i++)
+    getline(cin,msg);
+
+    for(int i = 0; i < msg.size(); i++)
       {
-	cout<<msg[i]<<endl;
+        if(msg[i] >= char(65) && msg[i] <= char(90) || msg[i] >= char(97) && msg[i] <= char(122))
+        cout << char(msg[i] + key);
+        else
+          cout <<msg[i];
+      }
+  return;
+  }
+
+void decode()
+  {
+    string msg;
+    int key = 13;
+    cout<<"Please Enter your Encrypted Message: ";
+    getline(cin, msg);
+    for(int i = 0; i < msg.size(); i++)
+      {
+        if(msg[i] > char(64) && msg[i] < char(93) || msg[i] > char(96) && msg[i] < char(123))
+        cout << char(msg[i] - key);
+        else
+          cout<<msg[i];
       }
   }
