67557f315876f16f9e533adaf3658f65b87f60c6
Ahmed Waheed <34095723+aw0reborn@users.noreply.github.com>
2018-02-28 17:08:40 +0500

    Update rot13.cpp

--- rot13.cpp
+++ rot13.cpp
@@ -21,38 +21,3 @@ void choices() //Prompts Options to the user
 int main()  // My Main function
   {
     choices();
-  }
-
-void encode()  //Starts Encoding
-  {
-    string msg;
-    int key = 13;
-    cout<<"Please Enter the plain text to Encrypt: ";
-    getline(cin,msg);     //Takes plain text and saves it into msg
-
-    for(int i = 0; i < msg.size(); i++)
-      {
-        if(msg[i] >= char(65) && msg[i] <= char(77) || msg[i] >= char(97) && msg[i] <= char(109))
-          cout<< char(msg[i] + key);
-        else if(msg[i] > char(77) && msg[i] <= char(90) || msg[i] > char(109) && msg[i] <= char(122))
-          cout<< char(msg[i] - key);
-        else                                                //for special characters
-          cout<<msg[i];
-      }
-  return;
-  }
-
-void decode()  //Starts Decoding
-  {
-    string msg;
-    int key = 13;
-      cout<<"Please Enter your Encrypted Message: ";
-      getline(cin, msg);
-    for(int i = 0; i < msg.size(); i++)
-      {
-        if(msg[i] >= char(65) && msg[i] <= char(77) || msg[i] >= char(97) && msg[i] <= char(109))
-          cout << char(msg[i] - key);
-        else if(msg[i] > char(77) && msg[i] <= char(90) || msg[i] > char(109) && msg[i] <= char(122))
-          cout<<char(msg[i] - key);
-      }
-  }
